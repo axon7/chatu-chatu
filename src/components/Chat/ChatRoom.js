85f33f582794df8c8b72cf2f35ee5a5a0123bc19
Kuba <kubaz9991@gmail.com>
2019-07-21 22:51:50 +0200

    express installed

--- src/components/Chat/ChatRoom.js
+++ src/components/Chat/ChatRoom.js
@@ -1,7 +1,7 @@
 import React from "react";
 import io from "socket.io-client";
 import ChatTable from "./ChatTable";
-const socketURL = "http://localhost:3456";
+const socketURL = "/";
 const socket = io(socketURL);
 class ChatRoom extends React.Component {
   constructor(props) {
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,14 @@
-const app = require("http").createServer();
+const express = require("express");
+
+const app = express();
+const server = require("http").Server(app);
 // This creates our socket using the instance of the server
 const io = (module.exports.io = require("socket.io")(app));
 
 let port = process.env.port || 3456;
 
 // const SocketManager = require("./SockerManager.js");
+app.use(express.static(__dirname + "/../../build"));
 
 io.on("connection", socket => {
   console.log("user connected id: " + socket.id);
@@ -20,6 +24,6 @@ io.on("connection", socket => {
   });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("server running on port: " + port);
 });
