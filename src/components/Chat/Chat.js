05a318d0f97e33bee4e942b5c43ee30864fe917c
Kuba <kubaz9991@gmail.com>
2019-08-03 11:14:46 +0200

    port for production

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 import ChatName from "./ChatName";
 import ChatRoom from "./ChatRoom";
-const socketURL = "localhost:3456";
+const socketURL = "/";
 
 const Wrapper = styled.div`
   display: flex;
